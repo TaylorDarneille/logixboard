3f12858db32f2bde2928c8de9dccc00ee0b46597
Taylor Darneille <tmdarneille@gmail.com>
2020-11-11 11:25:19 -0800

    App.js is now a class component that stores the client name in state and retrieves shipment data based on that client name, printed to console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    client: 'Broko'
+  }
+
+  componentDidMount() {
+    this.fetchShipments()
+  }
+  
+  fetchShipments = () => {
+    let query_params = new URLSearchParams({
+      // 'limit': 10,
+      'query_type': 'and',
+      'Client Name': this.state.client,
+    })
+    let url = 'https://sheet2api.com/v1/OW9YmRzCQ3zF/logixboard_shipment_data/Sheet1?' + query_params;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data)
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>LogixBoard Shipment Data</h1>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
