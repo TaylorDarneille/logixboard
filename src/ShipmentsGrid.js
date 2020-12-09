54aec16bd6d3618b8ed81aa8c657d9a9d315b56d
Taylor Darneille <tmdarneille@gmail.com>
2020-12-09 08:25:36 -0800

    added ShipmentsGrid as Shipments v2, made columns more generic, added comments

--- src/ShipmentsGrid.js
+++ src/ShipmentsGrid.js
@@ -1,18 +1,36 @@
 import React, { useEffect } from 'react'
 import { useTable } from 'react-table'
  
-function Shipments(props) {
+function ShipmentsGrid(props) {
 
-    useEffect(()=>{
-        console.log('props.filteredShipments:')
-        console.log(props.filteredShipments)
-    })
+    // ----------------------------------------------
+    // this is just to see the filteredShipments prop
+    // provides no functionality
+    // ----------------------------------------------
+    // useEffect(()=>{
+    //     console.log('props.filteredShipments:')
+    //     console.log(props.filteredShipments)
+    // })
 
+    // ----------------------------------------------
+    // memoize "data" as props.filteredShipments
+    // only updates data when props.filteresShipments changes
+    // this is purely for performance optimization
+    // ----------------------------------------------
     const data = React.useMemo(
         () => props.filteredShipments,
         [props.filteredShipments]
     )
 
+    // ----------------------------------------------
+    // memoize "columns", which is used in the useTable hook
+    // Header: column header in table
+    // accessor: key from "data" that corresponds to column header
+    // Header does not have to be the same as accessor
+    // this piece of code is purely for scalability/flexibility
+    // memoizing columns *really* only becomes useful if
+    // the structure of data might be changing (ie diff clients have diff cols)
+    // ----------------------------------------------
     const columns = React.useMemo(
         () => {
             let cols = []
@@ -38,19 +56,19 @@ function Shipments(props) {
             <thead>
             {headerGroups.map(headerGroup => (
                 <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                    <th
-                    {...column.getHeaderProps()}
-                    style={{
-                        borderBottom: 'solid 3px red',
-                        background: 'aliceblue',
-                        color: 'black',
-                        fontWeight: 'bold',
-                    }}
-                    >
-                    {column.render('Header')}
-                    </th>
-                ))}
+                    {headerGroup.headers.map(column => (
+                        <th
+                        {...column.getHeaderProps()}
+                        style={{
+                            borderBottom: 'solid 3px red',
+                            background: 'aliceblue',
+                            color: 'black',
+                            fontWeight: 'bold',
+                        }}
+                        >
+                        {column.render('Header')}
+                        </th>
+                    ))}
                 </tr>
             ))}
             </thead>
@@ -81,4 +99,4 @@ function Shipments(props) {
         )
     }
 
-export default Shipments
+export default ShipmentsGrid
